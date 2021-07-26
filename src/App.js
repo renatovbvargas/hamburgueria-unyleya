2134ffe906f97aefd7a6d22108c1cb396a38ed94
Renato Vilas Boas Vargas <rvargas@petros.com.br>
2021-07-25 21:42:44 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+
+function Routes() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path='/' exact={true} component={Home} />
+        <Route path='/cardapio' exact={true} component={Menu} />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default Routes;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <footer>
+            <h1>Rodapé</h1>
+        </footer>
+    )
+}
+
+export default Footer;
